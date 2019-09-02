ee1714b9194a668092d7f91efdafcf7eebc29b48
ojullien <ojullien@users.noreply.github.com>
2019-09-02 20:23:52 +0200

    wip

---
+++ src/module/App/Exception/BadMethodCallException.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is a part of the Pomponne version of Pbraiders
+ *
+ * @package Pbraiders\App\Exception
+ * @link    https://github.com/pbraiders/pomponne for the canonical source repository
+ * @license https://github.com/pbraiders/pomponne/blob/master/LICENSE GNU General Public License v3.0 License.
+ */
+
+namespace Pbraiders\App\Exception;
+
+/**
+ * Exception thrown if a callback refers to an undefined method or if some arguments are missing.
+ * This represents error in the program logic and should be detected at compile time.
+ * This kind of exceptions should directly lead to a fix in the code.
+ */
+class BadMethodCallException extends \BadMethodCallException implements ExceptionInterface
+{
+
+}
---
+++ src/module/App/Exception/DomainException.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * This file is a part of the Pomponne version of Pbraiders
+ *
+ * @package Pbraiders\App\Exception
+ * @link    https://github.com/pbraiders/pomponne for the canonical source repository
+ * @license https://github.com/pbraiders/pomponne/blob/master/LICENSE GNU General Public License v3.0 License.
+ */
+
+namespace Pbraiders\App\Exception;
+
+/**
+ * Exception thrown if a value does not adhere to a defined valid data domain.
+ * This represents error in the program logic and should be detected at compile time.
+ * This kind of exceptions should directly lead to a fix in the code.
+ */
+class DomainException extends \DomainException implements ExceptionInterface
+{
+
+}
