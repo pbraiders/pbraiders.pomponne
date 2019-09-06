7dcdfbbed28ac7c7d4021b453513c44fbba62100
ojullien <ojullien@users.noreply.github.com>
2019-09-06 15:17:12 +0200

    wip

--- index.php
+++ index.php
@@ -9,7 +9,10 @@ declare(strict_types=1);
  * @license https://github.com/pbraiders/pomponne/blob/master/LICENSE GNU General Public License v3.0 License.
  */
 
-// @var string Root path.
-define('PBR_PATH', realpath(__DIR__));
+/** @var ROOT_PATH document root path */
+define('ROOT_PATH', realpath(__DIR__));
 
-require \PBR_PATH . \DIRECTORY_SEPARATOR . 'module' . \DIRECTORY_SEPARATOR . 'bootstrap.php';
+/** @var PBR_PATH PBR path */
+define('PBR_PATH', \ROOT_PATH . \DIRECTORY_SEPARATOR . 'pbr');
+
+require \PBR_PATH . \DIRECTORY_SEPARATOR . 'bootstrap.php';
--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,8 +2,11 @@
 
 declare(strict_types=1);
 
-// @var string Root path.
-defined('PBR_PATH') || define('PBR_PATH', realpath(dirname(__DIR__)));
+/** @var ROOT_PATH document root path */
+defined('ROOT_PATH') || define('ROOT_PATH', realpath(dirname(__DIR__)));
+
+/** @var PBR_PATH PBR path */
+defined('PBR_PATH') || define('PBR_PATH', \ROOT_PATH . \DIRECTORY_SEPARATOR . 'pbr');
 
 // Includes the Composer autoloader
-require realpath(\PBR_PATH . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php');
+require realpath(\ROOT_PATH . \DIRECTORY_SEPARATOR . 'vendor' . \DIRECTORY_SEPARATOR . 'autoload.php');
